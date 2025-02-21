4cee67dc5ab52f730545793d48c6bb01273bb67f
MykolaDekusar <ndekusar97@gmail.com>
2025-02-22 00:33:30 +0100

    UPD

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  if (days <= 0) {
+    return error;
+  }
+  let total = days * 40;
+
+  if (days >= 3 && days < 7) {
+    return total - 20;
+  }
+  if (days >= 7) {
+    return total - 50;
+  }
+  return total;
 }
 
 module.exports = calculateRentalCost;
